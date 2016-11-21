7ec115d976a3366281a59c06e41e41d5b650a60d
WYseven <consolelog@126.com>
2016-11-21 13:04:23 +0800

    添加了登录跳转

--- routes/api-route.js
+++ routes/api-route.js
@@ -5,20 +5,20 @@ var express = require("express");
 var router = express.Router();
 
 
-router.get("/login",function (req,res) {
+router.get("/login",function(req,res){
+    var userName = req.query.userName;
+    var usersColletion =  db.collection("user");
 
-    db.collection("user").findOne(function(err,data){
-        console.log(data);
+    usersColletion.findOne({userName:userName},function(err,data){
+        if( err ) throw err;
+        res.json({
+            status: 1,
+            message:"已经存在"
+        });
     })
-
-    res.json({
-        status: 'ok'
-    });
-});
-
+})
 router.get("/reg",function (req,res) {
     var userName = req.query.userName;
-    console.log(req.query);
     var usersColletion =  db.collection("user");
     usersColletion.findOne({userName:userName},function(err,data){
         if( err ){
@@ -27,14 +27,16 @@ router.get("/reg",function (req,res) {
             console.log(data);
             if(data){
                 res.json({
-                    status: '已经存在'
+                    status: 1,
+                    message:"已经存在"
                 });
             }else{
                 usersColletion.insert({
                     userName:userName
                 }).then(function(){
                     res.json({
-                        status: '注册成功'
+                        status: 0,
+                        message:"注册成功"
                     });
                 }).catch(function(err){
                     console.log(err);
@@ -43,9 +45,9 @@ router.get("/reg",function (req,res) {
             }
         }
     })
-
-
 });
+
+
 
 
 module.exports = router;
